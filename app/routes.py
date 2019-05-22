8bc21ac41242706f94bdb7bb24c66d94f9140563
Andy Webster <awframpton@gmail.com>
2019-05-22 04:44:03 -0500

    checkin

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,13 @@ from app.forms import RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -59,3 +66,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user,'body': 'Test post #1'},
+        {'author': user,'body': 'Test post #2'}
+    ]
+    return render_template('user.html',user=user, posts=posts)
